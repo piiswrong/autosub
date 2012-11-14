1217dbcdbf6d88f74c32ad1d54c5d0b63b0273a9
abcxys <abcxys92@gmail.com>
2012-11-14 17:05:54 +0800

    add two buttons to manipulate the two borders

--- src/ui/spectrum_widget/imOnScrWin.py
+++ src/ui/spectrum_widget/imOnScrWin.py
@@ -1,3 +1,4 @@
+# -*- coding: cp936 -*-
 import wx
 from pylab import *
 
@@ -21,7 +22,6 @@ while handle.more_data():
         iter = 0
         q = []
         pos, n, q = handle.read(1000, q)
-        path = "D:\\activities\\ASE\\Group\\spectrogram\\spec" + str(Num) +".jpg"
         for p in q:
                 if iter == 0:
                         temp = p.T[::-1]
@@ -38,7 +38,6 @@ while handle.more_data():
         vector = np.dstack(vector)
         #plt.imsave(path, vector, cmap = cm.gray)
         im = wx.ImageFromBuffer(int(np.size(vector, axis = 1)), int(np.size(vector, axis = 0)), np.uint8(vector))
-        im.SaveFile(path, wx.BITMAP_TYPE_JPEG)
         if Num == 0:
                 specW = vector
                 list = [vector]
@@ -71,21 +70,10 @@ class ImageWindow(wx.ScrolledWindow):
     def SetBitmap(self, bitmap):
         self.bitmap = bitmap
         self.buffer = wx.EmptyBitmap(bitmap.GetWidth(), bitmap.GetHeight()+15)
-        #self.SetVirtualSize(bitmap.GetSize())
- 
-        # The following does nothing when called before
-        # the app starts.
-        #cdc = wx.ClientDC(self)
-        #cdc.DrawBitmap(self.bitmap, 0, 150- self.bitmap.GetHeight())
         
         
     def OnPaint(self, event):
         #dc = wx.BufferedPaintDC(self, self.bitmap)
-        """if self.bitmap.GetHeight() > 150:
-            rect = wx.Rect(0, self.bitmap.GetHeight() - 150, self.bitmap.GetWidth(), 150)
-            dc = wx.BufferedPaintDC(self, self.bitmap.GetSubBitmap(rect), wx.BUFFER_VIRTUAL_AREA)
-        else:
-            dc = wx.BufferedPaintDC(self, self.bitmap, wx.BUFFER_VIRTUAL_AREA)"""
         dc = wx.BufferedPaintDC(self, self.buffer, wx.BUFFER_VIRTUAL_AREA)
         dc.DrawBitmap(self.bitmap, 0, 0)
         odc=wx.DCOverlay(self.overlay, dc)
@@ -121,26 +109,10 @@ class ImageWindow(wx.ScrolledWindow):
                 font = wx.Font(7, wx.FONTFAMILY_ROMAN, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_LIGHT)
                 dc.SetFont(font)
                 dc.DrawText(str(i), i, self.bitmap.GetHeight()+5)
-        print -self.CalcScrolledPosition(0,0)[0]
         event.Skip()
-        #self.DrawAxis(dc)
-        #dc1 = wx.ClientDC(self)
-        #self.DrawAxis(dc1)
-        #dc.DrawLine(self.bitmap.GetWidth()/2.0, 0, self.bitmap.GetWidth()/2.0, 150)
-
-        ## I have tried it with and without the following two lines
-        #x,y = self.GetViewStart()
-        #dc.DrawBitmap(self.bitmap, x, y)
             
     def OnScroll(self, evt):
         self.ScrollFlag = 1
-        """a = self.GetScrollPos(wx.HORIZONTAL)/1010
-        #print a
-        self.image = wx.ImageFromBuffer(int(np.size(vector, axis = 1)), int(np.size(vector, axis = 0)), np.uint8(vector))
-        self.bitmap = self.image.ConvertToBitmap()
-        cdc = wx.ClientDC(self)
-        cdc.DrawBitmap(self.bitmap, 1000 * a, 150-self.bitmap.GetHeight())
-        #self.image = wx.ImageFromBuffer(int(np.size(list[], axis = 1)), int(np.size(list[0], axis = 0)), np.uint8(list[0]))"""
         evt.Skip()
 
     def OnMouseClick(self, event):
@@ -149,11 +121,6 @@ class ImageWindow(wx.ScrolledWindow):
         self.LeX=event.X -self.CalcScrolledPosition(0,0)[0]
         self.Refresh()
         #self.CaptureMouse()
-        """odc=wx.DCOverlay(self.overlay, dc)
-        odc.Clear()
-        dc.SetPen(wx.Pen('red', 1))
-        dc.DrawLine(self.ReX-self.CalcScrolledPosition(0,0)[0],0,
-                            self.ReX-self.CalcScrolledPosition(0,0)[0], 150)"""
         #del odc
         event.Skip()
 
@@ -179,10 +146,15 @@ class Panel1(wx.Frame):
                         size=(55, 150), style=wx.SL_VERTICAL | wx.SL_AUTOTICKS | wx.SL_LABELS)
         self.sld.SetTickFreq(20, 1)
         self.sld1 = wx.Slider(panel, value = 200, minValue = 150, maxValue =500,pos = (70,10),
-                        size=(80, 150), style=wx.SL_VERTICAL| wx.SL_AUTOTICKS | wx.SL_LABELS )
+                        size=(50, 150), style=wx.SL_VERTICAL| wx.SL_AUTOTICKS | wx.SL_LABELS )
         self.sld1.SetTickFreq(20, 1)
-        
-        
+
+
+        #ADD TWO BUTTONS TO MANIPULATE THE LEFT AND RIGHT BORDER
+        self.button1 = wx.Button(panel, id=1, label='left', pos = (120,10), size = (70,25))
+        self.button2 = wx.Button(panel, id=2, label='right', pos=(120, 40), size = (70,25))
+        self.button1.Bind(wx.EVT_BUTTON, self.LeftButton)
+        self.button2.Bind(wx.EVT_BUTTON, self.RightButton)
         
         self.wind.SetSize((300,150))
         self.wind.SetBitmap(self.im.ConvertToBitmap())
@@ -191,12 +163,7 @@ class Panel1(wx.Frame):
         self.wind.FitInside()
         self.wind.SetScrollbars(1,0, self.im.GetWidth(), 400)
         
-        
-        """self.ruler=RC.RulerCtrl(panel, -1, pos=(0, np.size(specW, axis=0)), size=(30000,1),orient=wx.HORIZONTAL, style=wx.NO_BORDER)
-        self.ruler.SetFlip(flip=True)
-        self.ruler.SetRange(0, self.im.GetWidth())
-        #self.ruler.SetFormat(RC.TimeFormat)
-        self.ruler.AddIndicator(1, 50)"""
+
         
         sizer = wx.BoxSizer ()
         sizer.Add(self.wind, 1, wx.EXPAND, 0)
@@ -217,7 +184,17 @@ class Panel1(wx.Frame):
         self.wind.SetBitmap(self.im.ConvertToBitmap())
 
     def sliderUpdate2(self, event):
-        pass        
+        pass
+
+    def LeftButton(self, event):
+        self.wind.LeX = self.wind.LeX -1
+        self.wind.Refresh()
+        event.Skip()
+
+    def RightButton(self, event):
+        self.wind.RiX = self.wind.RiX +1
+        self.wind.Refresh()
+        event.Skip()
 
 if __name__=='__main__':
     app = wx.PySimpleApp()
