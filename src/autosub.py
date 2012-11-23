f23cd28f21bc2f088ff619f3b5f71380d35118c8
junx1992 <junx1992@gmail.com>
2012-11-23 13:47:07 +0800

    change the time format

--- src/autosub.py
+++ src/autosub.py
@@ -46,16 +46,45 @@ class MainFrame(vlc_wx.MyFrame):
         vad.start()
         sub.start()
 
+    # float second to time string 
+    def OnFormatTime(self,floattime):
+        time=int(floattime)
+        str_decimaltime=str(int((floattime-time)*1000))
+        sec=time%60
+        all_min=time/60
+        hour=all_min/60
+        mini=all_min%60
+        if sec<9:
+            str_sec='0'+str(sec)
+        else:
+            str_sec=str(sec)
+        if mini<9:
+            str_min='0'+str(mini)
+        else:
+            str_min=str(mini)
+        if hour<9:
+            str_hour='0'+str(hour)
+        else:
+            str_hour=str(hour)
+
+        string_time=str_hour+':'+str_min+':'+str_sec+'.'+str_decimaltime
+        return string_time
+
     def OnTimer(self,evt):
         super(MainFrame, self).OnTimer(self)
         if self.ohandle.has_data(1):            
             (start,self.end,text)=self.ohandle.read(1)[2][0][0]            
             self.player.video_set_subtitle_file(self.subtitle)
+            str_start=self.OnFormatTime(start)
+            str_end=self.OnFormatTime(self.end)
+            self.subpanel.AddSub(self.subpanel,str_start,str_end,text)
         # Set buffer time
         if self.player.get_length()!=0:
             self.buffergauge.SetValue(self.end*self.buffergauge.GetRange()*1000/self.player.get_length())        
         # Link with subtitle
-        self.subpanel.OpenTheFile(self.subpanel,self.subtitle)
+        #self.subpanel.OpenFile(self.subtitle)
+
+ 
         
 if __name__ == '__main__':
         # Create a wx.App(), which handles the windowing system event loop
